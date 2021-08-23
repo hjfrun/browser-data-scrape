acebedf37a55df0f2f92eb85e52556ac25ceddc1
Jianfeng He <jianfeng.he@oliverwyman.com>
2021-08-23 23:09:53 +0800

    add bri

---
+++ Bank Rakyat Indonesia/request.js
@@ -0,0 +1,11 @@
+https://bri.co.id/en/lokasi?p_p_id=location_display_ortlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=%2Flocation%2Fui%2Fsearch&p_p_cacheability=cacheLevelPage
+
+
+https://bri.co.id/en/lokasi?p_p_id=location_display_ortlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=/location/ui/search&p_p_cacheability=cacheLevelPage
+
+https://bri.co.id/en/lokasi?p_p_id=location_display_ortlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=%2Flocation%2Fui%2Fcount-data&p_p_cacheability=cacheLevelPage
+
+// {
+//   "pagingSize": 18387,
+//   "totalRecord": 220643
+// }
